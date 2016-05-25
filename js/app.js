4a1862175f5ca51b146707f8d6dc9fd959776eb7
Josh Kuhar <jdkuhar@gmail.com>
2016-05-25 14:09:52 -0400

    commit message

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
-	$(".ryu").mouseenter(function) {
-		alert("mouse entered .ryu div");
-	}
+  $('.ryu').mouseenter(function() {
+    $('.ryu-still').hide();
+	$('.ryu-ready').show();
+  })
+  .mouseleave(function() {
+    $('.ryu-still').show();
+    $('.ryu-ready').hide();
+  });
 });
